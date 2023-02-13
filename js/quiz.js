c83130b431e7b7da798aab02ffcf45d72aefd780
Grace <yintingchancytg@gmail.com>
2023-02-13 14:48:16 +0100

    Added quiz page

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,13 @@
 "use strict"
 
 function showQuizPage() {
-  console.log("TEST");
+  loginPage.querySelector(".container").classList.remove("login-container");
+  loginPage.querySelector(".container").classList.add("quiz-container");
+  document.querySelector(".alert-text").textContent = "Getting a random image...";
+  document.querySelector("main").innerHTML = `
+    <img class="dog-background-image" src="media/logo.png" alt="dog_logo">
+  `;
+  setTimeout(() => {
+    document.querySelector(".alert-container").classList.remove("alert-container-visible");
+  }, 500);
 }
